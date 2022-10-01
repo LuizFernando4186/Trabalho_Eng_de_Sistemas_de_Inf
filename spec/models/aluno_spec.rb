d556f54486dc3683bd6e12912ce4a7f51355c98a
Vinicius Roland <vinicius.roland@btgpactual.com>
2022-09-30 22:09:31 -0300

    fix

--- spec/models/aluno_spec.rb
+++ spec/models/aluno_spec.rb
@@ -1,6 +1,3 @@
-require 'coveralls'
-Coveralls.wear!('rails')
-
 require 'rails_helper'
 
 RSpec.describe Aluno, type: :model do
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,6 @@
+require 'coveralls'
+Coveralls.wear!('rails')
+
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
