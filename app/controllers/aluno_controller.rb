b108e163a4b3ca65b0f44721e9ba9018e8669cfc
Jose Rafael R Nascimento <jose.rafael@usp.br>
2022-10-07 17:15:35 -0300

    Add Aluno Controller Test

--- app/controllers/aluno_controller.rb
+++ app/controllers/aluno_controller.rb
@@ -1,9 +1,11 @@
 class AlunoController < ApplicationController
+
   def index
     @aluno = Aluno.new({ :name => "Jose Valim", :email => "teste@gmail.com", :nusp => 10773381})
     puts @aluno.inspect
     @aluno.save
   end
+
 
   def list
     alunos = Aluno.all
@@ -11,4 +13,5 @@ class AlunoController < ApplicationController
     puts alunos.inspect
     render json: alunos
   end
+
 end
