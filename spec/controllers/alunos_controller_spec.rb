275180e9a8dfccd2baef2e0498e8bedb613e97bb
Jose Rafael R Nascimento <jose.rafael@usp.br>
2022-10-07 17:49:23 -0300

    Fix controller test

---
+++ spec/controllers/alunos_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Aluno, :type => :request do
+  describe "Request home page" do
+    it "should be successful" do
+      get alunos_path
+      expect(response).to be_successful
+      expect(response.body).to include("Jose Valim")
+    end
+  end
+end
