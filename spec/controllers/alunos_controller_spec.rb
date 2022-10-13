34cc81f9e60462d20362c90d2787967d9cb50f7a
Jose Rafael R Nascimento <jose.rafael@usp.br>
2022-10-13 11:24:01 -0300

    Fix RSpec aluno

--- spec/controllers/alunos_controller_spec.rb
+++ spec/controllers/alunos_controller_spec.rb
@@ -2,17 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Aluno, :type => :request do
 
-  describe "Request home page" do
-    it "should be successful" do
-      get alunos_path, params: { :id => 1 }
-      expect(response).to be_successful
-    end
-  end
-
   describe "Add Aluno" do
     it "Should add a new Aluno" do
-      user = Aluno.create(name: "Teste RSPEC", email: "teste-rspec@gmail.com", nusp: "99999999")
-      get alunos_path
+      user = Aluno.create(nome: "Teste RSPEC", email: "teste-rspec@gmail.com")
+      get alunos_path, params: { :id => 1 }
       expect(response).to be_successful
       expect(response.body).to include("Teste RSPEC")
     end
