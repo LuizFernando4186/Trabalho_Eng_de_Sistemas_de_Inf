aa773d3cf7674ba482f82252fa487657553e7629
Jose Rafael R Nascimento <jose.rafael@usp.br>
2022-10-21 18:10:24 -0300

    Update GitHub Actions only to branch main

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,13 @@ Rails.application.routes.draw do
   resources :professors
   resource :provas
   resource :alunos
-  resource :questaos
-
+  #resource :questaos, only: [:show, :new, :create, :edit, :update, :destroy]
+  #resource :questaos, except: [:index]
+  
   get 'alunos/all'
   get 'provas/all'
   get 'login/index'
+  get '/questaos', to: 'questaos#index'
+  get '/questaos/new', to: 'questaos#new'
 
 end
