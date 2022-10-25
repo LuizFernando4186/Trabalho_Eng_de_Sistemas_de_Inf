0dfa319b22536cdedbcdd536b61ee2cc5951663e
Jose Rafael R Nascimento <jose.rafael@usp.br>
2022-10-25 17:27:50 -0300

    Aumenta validacao da questao fechada

--- app/models/questao_fechada.rb
+++ app/models/questao_fechada.rb
@@ -3,5 +3,9 @@ class QuestaoFechada < ApplicationRecord
     validates :grupo_questao, presence: { message: "É obrigatório informar o grupo da questão!" }
     validates :titulo, presence: { message: "É obrigatório informar o título da questão!" }
     validates :enunciado, presence: { message: "É obrigatório informar o enunciado da questão!" }
+    validates :total_alternativas, presence: { message: "É obrigatório informar o total de alternativas da questão!" }
+    validates :alternativas_aluno, presence: { message: "É obrigatório informar o total de alternativas que o aluno deve marcar!" }
+    validates :alternativas, presence: { message: "É obrigatório informar as alternativas da questão!" }
+    validates :alternativa_correta, presence: { message: "É obrigatório informar a alternativa correta da questão!" }
 
 end
