810d9c94c39d3918291aed2f3d3ea812d9a8bf99
Vinicius Roland <vinicius.roland@btgpactual.com>
2022-11-05 15:03:29 -0300

    add nova rota de notas

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@ Rails.application.routes.draw do
   root to: 'home#index'
   resource :provas
   resource :alunos
+  resource :notas
   resources :questaos
 
   get 'alunos/all'
   get 'provas/all'
+  get 'notas/all'
   get 'home', to: "home#index"
   get 'sign_in', to: "login#index"
   post 'sign_in', to: "login#create"
