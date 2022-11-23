0aed203c1de7242696fc918c7fa7545ae7e00ae4
J Rafael R Nascimento <63913814+jrafaelrn@users.noreply.github.com>
2022-11-23 18:35:15 -0300

    change association

--- spec/factories/questao_fechada.rb
+++ spec/factories/questao_fechada.rb
@@ -2,15 +2,12 @@ FactoryBot.define do
   
   factory :questao_fechada do
 
-    after :create do |questao|
-      create_list :alternativa, 4, questao_id: questao
-    end
-
     grupo_questao { "Grupo 1" }
     titulo { "Meu titulo" }
     enunciado { "Meu enunciado" }
     total_alternativas { 4 }
     alternativas_aluno { 3 }
+    alternativas { association :alternativa }
     alternativa_correta { 1 }
 
   end
