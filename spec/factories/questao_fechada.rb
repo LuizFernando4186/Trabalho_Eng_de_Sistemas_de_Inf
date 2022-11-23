b7b690274dfa95cdb395553ac2bf0dc9d8c8ff3a
J Rafael R Nascimento <63913814+jrafaelrn@users.noreply.github.com>
2022-11-23 18:20:05 -0300

    corrige relação um-muitos questoes-alternativas

--- spec/factories/questao_fechada.rb
+++ spec/factories/questao_fechada.rb
@@ -1,11 +1,18 @@
 FactoryBot.define do
+  
   factory :questao_fechada do
+
+    after :create do |questao|
+      create_list(:alternativa, 4, questao: questao)
+    end
+
     grupo_questao { "Grupo 1" }
     titulo { "Meu titulo" }
     enunciado { "Meu enunciado" }
     total_alternativas { 4 }
     alternativas_aluno { 3 }
-    association :alternativas, factory: :alternativa
     alternativa_correta { 1 }
+    
   end
+
 end
