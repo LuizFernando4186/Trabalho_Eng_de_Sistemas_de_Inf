a611c73f8197d1467ee1ea13e53bdcdfc3b86f66
J Rafael R Nascimento <63913814+jrafaelrn@users.noreply.github.com>
2022-11-23 15:51:11 -0300

    Adicionado SPEC para model Questao

--- spec/models/questao_spec.rb
+++ spec/models/questao_spec.rb
@@ -1,5 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe Questao, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  context "Validações" do
+    it "é válido com todos os atributos" do
+      questao = Questao.new(
+        grupo_questao: "Grupo de Questão",
+        tipo: "Tipo de Questão",
+        titulo: "Título da Questão",
+        enunciado: "Enunciado da Questão"
+      )
+      expect(questao).to be_valid
+    end
+
+    it "é inválido sem grupo de questão" do
+      questao = Questao.new(
+        grupo_questao: nil,
+        tipo: "Tipo de Questão",
+        titulo: "Título da Questão",
+        enunciado: "Enunciado da Questão"
+      )
+      expect(questao).to_not be_valid
+    end
+
+    it "é inválido sem tipo de questão" do
+      questao = Questao.new(
+        grupo_questao: "Grupo de Questão",
+        tipo: nil,
+        titulo: "Título da Questão",
+        enunciado: "Enunciado da Questão"
+      )
+      expect(questao).to_not be_valid
+    end
+
+    it "é inválido sem título de questão" do
+      questao = Questao.new(
+        grupo_questao: "Grupo de Questão",
+        tipo: "Tipo de Questão",
+        titulo: nil,
+        enunciado: "Enunciado da Questão"
+      )
+      expect(questao).to_not be_valid
+    end
+
+    it "é inválido sem enunciado de questão" do
+      questao = Questao.new(
+        grupo_questao: "Grupo de Questão",
+        tipo: "Tipo de Questão",
+        titulo: "Título da Questão",
+        enunciado: nil
+      )
+      expect(questao).to_not be_valid
+    end
+    
+  end
+
 end
