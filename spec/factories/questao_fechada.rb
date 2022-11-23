12bafe5606b8c06d9dc5d9661a746ac7e86ab7b4
J Rafael R Nascimento <63913814+jrafaelrn@users.noreply.github.com>
2022-11-23 19:14:24 -0300

    tentativa de criar questao e alternativa no mesmo factory

--- spec/factories/questao_fechada.rb
+++ spec/factories/questao_fechada.rb
@@ -1,11 +1,5 @@
 FactoryBot.define do
-
-  factory :alternativa do
-    alternativa { "Minha Alternativa" }
-    correta { true }
-    questao_id { nil }
-  end
-  
+ 
   factory :questao_fechada do
 
     grupo_questao { "Grupo 1" }
@@ -14,16 +8,21 @@ FactoryBot.define do
     total_alternativas { 4 }
     alternativas_aluno { 3 }
     alternativa_correta { 1 }
-    association :alternativa
+    alts
 
   end
 
+  factory :alts do
+    alternativa { "Minha Alternativa" }
+    correta { true }
+    questao_id { nil }
+  end
   
-  #def alternativas_questao(num_alternativas: 4)
-    #FactoryBot.create(:alternativa) do |alt|
-      #FactoryBot.create_list(:alternativa, num_alternativas, alternativa: alt)
-    #end
-  #end
+  def alternativas_questao(num_alternativas: 4)
+    FactoryBot.create(:alts) do |alts|
+      FactoryBot.create_list(:questao_fechada, num_alternativas, alts: alts)
+    end
+  end
 
 
 end
