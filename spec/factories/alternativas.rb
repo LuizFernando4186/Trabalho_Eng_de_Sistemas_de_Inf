f1b5f237b0aa04dffc80545981bd30e80881ef7b
J Rafael R Nascimento <63913814+jrafaelrn@users.noreply.github.com>
2022-11-23 17:58:24 -0300

    atualização do Factory para melhorar debug

--- spec/factories/alternativas.rb
+++ spec/factories/alternativas.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :alternativa do
-    alternativa { "MyText" }
+    alternativa { "Minha Alternativa" }
     correta { false }
     questao { nil }
   end
--- spec/factories/questao_fechada.rb
+++ spec/factories/questao_fechada.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :questao_fechada do
     grupo_questao { "Grupo 1" }
-    titulo { "MyString" }
-    enunciado { "MyText" }
+    titulo { "Meu titulo" }
+    enunciado { "Meu enunciado" }
     total_alternativas { 4 }
     alternativas_aluno { 3 }
-    alternativas { "MyText" }
+    alternativas { "Minhas alternativas..." }
     alternativa_correta { 1 }
   end
 end
