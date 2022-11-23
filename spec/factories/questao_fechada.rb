2468b3e209f208109818f9f1a7206d6f5f7b4a88
J Rafael R Nascimento <63913814+jrafaelrn@users.noreply.github.com>
2022-11-23 17:50:52 -0300

    corrige nome factory

--- spec/factories/questao_fechada.rb
+++ spec/factories/questao_fechada.rb
@@ -1,11 +1,10 @@
 FactoryBot.define do
   factory :questao do
-    grupo_questao { "" }
-    tipo { "MyString" }
+    grupo_questao { "Grupo 1" }
     titulo { "MyString" }
     enunciado { "MyText" }
-    total_alternativas { 1 }
-    alternativas_aluno { 1 }
+    total_alternativas { 4 }
+    alternativas_aluno { 3 }
     alternativas { "MyText" }
     alternativa_correta { 1 }
   end
