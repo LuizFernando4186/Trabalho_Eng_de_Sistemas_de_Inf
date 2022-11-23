eedbb198657c864411502d3a7c25c8f2533d38ea
J Rafael R Nascimento <63913814+jrafaelrn@users.noreply.github.com>
2022-11-23 17:16:44 -0300

    Add mais testes RSpec para modelo e correção de parametros

--- spec/models/alternativa_spec.rb
+++ spec/models/alternativa_spec.rb
@@ -1,57 +1,92 @@
 require 'rails_helper'
 
-RSpec.describe Questao, type: :model do
+RSpec.describe QuestaoFechada, type: :model do
   
   context "Validações" do
     it "é válido com todos os atributos" do
-      questao = Questao.new(
+      questao = QuestaoFechada.new(
         grupo_questao: "Grupo de Questão",
-        tipo: "Tipo de Questão",
         titulo: "Título da Questão",
-        enunciado: "Enunciado da Questão"
+        enunciado: "Enunciado da Questão",
+        total_alternativas: 4,
+        alternativas_aluno: 3,
+        alternativa_correta: 1
       )
       expect(questao).to be_valid
     end
 
     it "é inválido sem grupo de questão" do
-      questao = Questao.new(
+      questao = QuestaoFechada.new(
         grupo_questao: nil,
-        tipo: "Tipo de Questão",
         titulo: "Título da Questão",
-        enunciado: "Enunciado da Questão"
-      )
-      expect(questao).to_not be_valid
-    end
-
-    it "é inválido sem tipo de questão" do
-      questao = Questao.new(
-        grupo_questao: "Grupo de Questão",
-        tipo: nil,
-        titulo: "Título da Questão",
-        enunciado: "Enunciado da Questão"
+        enunciado: "Enunciado da Questão",
+        total_alternativas = 4,
+        alternativas_aluno = 3,
+        alternativa_correta = 1
       )
       expect(questao).to_not be_valid
     end
 
     it "é inválido sem título de questão" do
-      questao = Questao.new(
+      questao = QuestaoFechada.new(
         grupo_questao: "Grupo de Questão",
-        tipo: "Tipo de Questão",
         titulo: nil,
-        enunciado: "Enunciado da Questão"
+        enunciado: "Enunciado da Questão",
+        total_alternativas = 4,
+        alternativas_aluno = 3,
+        alternativa_correta = 1
       )
       expect(questao).to_not be_valid
     end
 
     it "é inválido sem enunciado de questão" do
-      questao = Questao.new(
+      questao = QuestaoFechada.new(
         grupo_questao: "Grupo de Questão",
-        tipo: "Tipo de Questão",
         titulo: "Título da Questão",
-        enunciado: nil
+        enunciado: nil,
+        total_alternativas = 4,
+        alternativas_aluno = 3,
+        alternativa_correta = 1
       )
       expect(questao).to_not be_valid
     end
+
+    it "é inválido sem total de alternativas" do
+      questao = QuestaoFechada.new(
+        grupo_questao: "Grupo de Questão",
+        titulo: "Título da Questão",
+        enunciado: "Enunciado da Questão",
+        total_alternativas = nil,
+        alternativas_aluno = 3,
+        alternativa_correta = 1
+      )
+      expect(questao).to_not be_valid
+    end
+
+    it "é inválido sem alternativas do aluno" do
+      questao = QuestaoFechada.new(
+        grupo_questao: "Grupo de Questão",
+        titulo: "Título da Questão",
+        enunciado: "Enunciado da Questão",
+        total_alternativas = 4,
+        alternativas_aluno = nil,
+        alternativa_correta = 1
+      )
+      expect(questao).to_not be_valid
+    end
+
+    it "é inválido sem alternativa correta" do
+      questao = QuestaoFechada.new(
+        grupo_questao: "Grupo de Questão",
+        titulo: "Título da Questão",
+        enunciado: "Enunciado da Questão",
+        total_alternativas = 4,
+        alternativas_aluno = 3,
+        alternativa_correta = nil
+      )
+      expect(questao).to_not be_valid
+    end
+
     
   end
 
