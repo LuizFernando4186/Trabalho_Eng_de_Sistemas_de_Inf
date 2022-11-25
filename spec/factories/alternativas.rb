e7914072a6d39f02a8552471ad4889efe50a955f
Jose Rafael R Nascimento <jose.rafael@usp.br>
2022-11-25 07:57:38 -0300

    Force questao_id Factory

--- spec/factories/alternativas.rb
+++ spec/factories/alternativas.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :alternativa do
-    alternativa { "Minha Alternativa" }
+    alternativa { "Minha Alternativa Factory" }
     correta { true }
-    questao_id { nil }
+    questao_id { 1 }
   end
 end
